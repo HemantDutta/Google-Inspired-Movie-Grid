7b70c297627c1deae5eedc8307d5b27e581f142d
HemantDutta <hemantnorddutta2@gmail.com>
2022-12-11 18:25:43 +0530

    show all logic

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,31 +1,41 @@
-function widen(id){
+function widen(id) {
     document.getElementById(`${id}`).classList.remove('col-lg-3');
     document.getElementById(`${id}`).classList.add('col-lg-6');
 }
 
-function shorten(id){
+function shorten(id) {
     document.getElementById(`${id}`).classList.remove('col-lg-6');
     document.getElementById(`${id}`).classList.add('col-lg-3');
 }
 
-function showDes(id, iid){
-        document.getElementById(`${id}`).classList.remove('d-none');
-        document.getElementById(`${id}`).classList.add('d-flex');
-        document.getElementById(`${iid}`).style.opacity = "50%"
-        document.getElementById(`${iid}`).style.filter = "blur(4px)"
+function showDes(id, iid) {
+    document.getElementById(`${id}`).classList.remove('d-none');
+    document.getElementById(`${id}`).classList.add('d-flex');
+    document.getElementById(`${iid}`).style.opacity = "50%"
+    document.getElementById(`${iid}`).style.filter = "blur(4px)"
 }
 
-function removeDes(id, iid){
+function removeDes(id, iid) {
     document.getElementById(`${id}`).classList.remove('d-flex');
     document.getElementById(`${id}`).classList.add('d-none');
     document.getElementById(`${iid}`).style.opacity = "100%"
     document.getElementById(`${iid}`).style.filter = "blur(0px)"
 }
 
-function showMovie(genre){
-        document.querySelectorAll('.movie').forEach(function (x){
-           if(!x.classList.contains(genre)){
-               x.classList.add('d-none');
-           }
+function showMovie(genre) {
+
+    if (genre === 'all') {
+        document.querySelectorAll('.movie').forEach(function (x) {
+            x.classList.remove('d-none');
         });
+    } else {
+        document.querySelectorAll('.movie').forEach(function (x) {
+            x.classList.remove('d-none');
+        });
+        document.querySelectorAll('.movie').forEach(function (x) {
+            if (!x.classList.contains(genre)) {
+                x.classList.add('d-none');
+            }
+        });
+    }
 }
